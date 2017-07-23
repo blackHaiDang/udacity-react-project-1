e15036776699ea8a91d7a71be1fb511246be8a43
xiaoju <me@xiaoju.io>
2017-07-23 12:52:38 +0200

    feat: filter books per shelf

--- src/AllMyBooks.js
+++ src/AllMyBooks.js
@@ -5,10 +5,12 @@ class AllMyBooks extends Component {
 			const { books } = this.props
       return (
         <div className="bookshelf">
-          <h2 className="bookshelf-title">Read</h2>
+          <h2 className="bookshelf-title">{this.props.ThisShelf}</h2>
           <div className="bookshelf-books">
             <ol className="books-grid">
-              {books.map((book) => (
+              {books
+              .filter((book) => (book.shelf === this.props.ThisShelf))
+              .map((book) => (
                 <li key={book.id}>
                   <div className="book">
                     <div className="book-top">
@@ -32,11 +34,9 @@ class AllMyBooks extends Component {
                     </div>
                     <div className="book-title">{book.title}</div>
                     <div className="book-authors">{book.authors[0]}</div>
-                    <div className="book-shelf">{book.shelf}</div>
                   </div>
                 </li>
-              )).filter((book) => (book.shelf === "currentlyReading"))
-
+              ))
             }
             </ol>
         </div>
