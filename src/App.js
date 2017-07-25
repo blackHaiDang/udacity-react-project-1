db110088d3e636f9a617231ebe328fb9520c0b35
xiaoju <me@xiaoju.io>
2017-07-25 14:39:19 +0200

    feat: add routing by react-router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 import EachShelf from './EachShelf'
+import { Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 
 class BooksApp extends React.Component {
   state = {
@@ -10,8 +12,7 @@ class BooksApp extends React.Component {
         "currentlyReading": [],
         "wantToRead": [],
         "read": []
-      },
-    showSearchPage: false
+      }
   }
 
   // shelvesDico = {
@@ -37,10 +38,11 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
+
+        <Route path="/new" render={() => (
           <div className="search-books">
             <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+              <Link to="/" className="close-search" >Close</Link>
               <div className="search-books-input-wrapper">
                 <input type="text" placeholder="Search by title or author"/>
               </div>
@@ -49,7 +51,9 @@ class BooksApp extends React.Component {
               <ol className="books-grid"></ol>
             </div>
           </div>
-        ) : (
+        )}/>
+
+        <Route exact path="/" render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -60,10 +64,13 @@ class BooksApp extends React.Component {
                 <EachShelf updateShelf={this.updateShelf} ShelfName="Read" ThisShelf={this.state.allShelves.read}/>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              <Link
+                to="/new"
+                >Add a book</Link>
             </div>
           </div>
-        )}
+        )}/>
+
       </div>
     )
   }
