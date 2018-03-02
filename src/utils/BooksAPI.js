c81d3f148700b1a51fc1a1d4b865ef0b910f41fe
xiaoju <me@xiaoju.io>
2018-03-03 00:08:15 +0100

    chore: remove unused test code

--- src/utils/BooksAPI.js
+++ src/utils/BooksAPI.js
@@ -1,40 +1,41 @@
-
-const api = "https://reactnd-books-api.udacity.com"
+const api = 'https://reactnd-books-api.udacity.com';
 
 /**
  * Since tests run without the dom (and thus without window.localStorage)
  *  create a storage placeholder only when tests are running to polyfill missing global
  */
-let token;
-let storage;
-
-if (process.env.NODE_ENV === 'test') {
-  token = null;
-  storage = { token:  null };
-} else {
-  token = localStorage.token;
-  storage = localStorage;
-}
+// let token;
+// let storage;
+//
+// if (process.env.NODE_ENV === 'test') {
+//   token = null;
+//   storage = { token:  null };
+// } else {
+//   token = localStorage.token;
+//   storage = localStorage;
+// }
 
 // Generate a unique token for storing your bookshelf data on the backend server.
-// let token = localStorage.token
+let token = localStorage.token;
 if (!token)
-  token = localStorage.token = Math.random().toString(36).substr(-8)
+  token = localStorage.token = Math.random()
+    .toString(36)
+    .substr(-8);
 
 const headers = {
-  'Accept': 'application/json',
-  'Authorization': token
-}
+  Accept: 'application/json',
+  Authorization: token
+};
 
-export const get = (bookId) =>
+export const get = bookId =>
   fetch(`${api}/books/${bookId}`, { headers })
     .then(res => res.json())
-    .then(data => data.book)
+    .then(data => data.book);
 
 export const getAll = () =>
   fetch(`${api}/books`, { headers })
     .then(res => res.json())
-    .then(data => data.books)
+    .then(data => data.books);
 
 export const update = (book, shelf) =>
   fetch(`${api}/books/${book.id}`, {
@@ -44,7 +45,7 @@ export const update = (book, shelf) =>
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ shelf })
-  }).then(res => res.json())
+  }).then(res => res.json());
 
 export const search = (query, maxResults) =>
   fetch(`${api}/search`, {
@@ -54,5 +55,6 @@ export const search = (query, maxResults) =>
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({ query, maxResults })
-  }).then(res => res.json())
-    .then(data => data.books)
+  })
+    .then(res => res.json())
+    .then(data => data.books);
